49c57f063a144e5f329bdd4b02c44804a0bf6de9
ymw19971129 <821872421@qq.com>
2024-06-03 15:12:46 +0800

    Update Quil.sh

--- Quil.sh
+++ Quil.sh
@@ -1,4 +1,4 @@
-#安装节点
+# 1.安装节点
 function install_node() {
 
 # 增加swap空间
@@ -64,10 +64,14 @@ Restart=always
 RestartSec=5s
 WorkingDirectory=/root/ceremonyclient/node
 ExecStart=/root/ceremonyclient/node/release_autorun.sh
-CPUQuota=$((`nproc` * 100))%
+CPUQuota=vcpuconut%
 
 [Install]
 WantedBy=multi-user.target' > /lib/systemd/system/ceremonyclient.service
+
+##更改cpu使用率
+vcpuconut=$(nproc)
+sed -i "s/CPUQuota=vcpuconut%/CPUQuota=$(($vcpuconut * 100))%/g"  /lib/systemd/system/ceremonyclient.service
 
 ##加载服务并启动
 sudo systemctl daemon-reload && sudo systemctl enable ceremonyclient
@@ -77,43 +81,114 @@ echo ====================================== 安装完成 请退出脚本使用sc
 
 }
 
-# 查看节点日志
+# 2.查看节点日志
 function check_service_status() {
     sudo journalctl -u ceremonyclient.service -f --no-hostname -o cat
 }
 
-# 重启节点
+# 3.配置监听端口8337和88338
+function listen_addr() {
+# 修改配置文件 .config/config.yml
+sed -i 's/listenGrpcMultiaddr:.*/listenGrpcMultiaddr: "\/ip4\/127.0.0.1\/tcp\/8337"/' ~/ceremonyclient/node/.config/config.yml
+sed -i 's/listenRESTMultiaddr:.*/listenRESTMultiaddr: "\/ip4\/127.0.0.1\/tcp\/8338"/' ~/ceremonyclient/node/.config/config.yml
+
+# 检查 .config/config.yml 配置结果
+grep -E 'listenGrpcMultiaddr|listenRESTMultiaddr' ~/ceremonyclient/node/.config/config.yml
+}
+
+# 4. 检查peerid
+function check_peerid() {
+	cd ~/ceremonyclient/node && ./node-1.4.18-linux-amd64 --peer-id
+}
+
+# 5. 获取PeerManifests信息
+function get_PeerManifests() {
+	# 修改配置文件 .config/config.yml
+	sed -i 's/listenGrpcMultiaddr:.*/listenGrpcMultiaddr: "\/ip4\/127.0.0.1\/tcp\/8337"/' ~/ceremonyclient/node/.config/config.yml
+	sed -i 's/listenRESTMultiaddr:.*/listenRESTMultiaddr: "\/ip4\/127.0.0.1\/tcp\/8338"/' ~/ceremonyclient/node/.config/config.yml
+
+	# 检查 .config/config.yml 配置结果
+	grep -E 'listenGrpcMultiaddr|listenRESTMultiaddr' ~/ceremonyclient/node/.config/config.yml
+	# 获取节点的 peer_id 并使用 base58 解码，再进行 base64 编码，并使用 grpcurl 获取 PeerManifests 信息
+	peer_id=$(grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNodeInfo | grep -o '"peerId": *"[^"]*"' | grep -o '"[^"]*"$' | tr -d '"' | base58 -d | base64) && grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetPeerManifests | grep -A 15 -B 1 "$peer_id"
+}
+
+# 6. 检查节点是否可见
+function check_vaild() {
+	bootstrap_peers=(
+	"EiDpYbDwT2rZq70JNJposqAC+vVZ1t97pcHbK8kr5G4ZNA=="
+	"EiCcVN/KauCidn0nNDbOAGMHRZ5psz/lthpbBeiTAUEfZQ=="
+	"EiDhVHjQKgHfPDXJKWykeUflcXtOv6O2lvjbmUnRrbT2mw=="
+	"EiDHhTNA0yf07ljH+gTn0YEk/edCF70gQqr7QsUr8RKbAA=="
+	"EiAnwhEcyjsHiU6cDCjYJyk/1OVsh6ap7E3vDfJvefGigw=="
+	"EiB75ZnHtAOxajH2hlk9wD1i9zVigrDKKqYcSMXBkKo4SA=="
+	"EiDEYNo7GEfMhPBbUo+zFSGeDECB0RhG0GfAasdWp2TTTQ=="
+	"EiCzMVQnCirB85ITj1x9JOEe4zjNnnFIlxuXj9m6kGq1SQ=="
+	)
+
+	# Run the grpcurl command and capture its output
+	output=$(grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNetworkInfo)
+
+	# Check if any of the specific peers are in the output
+	visible=false
+	for peer in "${bootstrap_peers[@]}"; do
+	    if [[ $output == *"$peer"* ]]; then
+	        visible=true
+	        echo "You see $peer as a bootstrap peer"
+	    else
+	        echo "Peer $peer not found"
+	    fi
+	done
+
+	if $visible ; then
+	    echo "Great, your node is visible!"
+	else
+	    echo "Sorry, your node is not visible. Please restart your node and try again."
+	fi
+}
+
+# 7.重启节点
 function restart_service() {
 	service ceremonyclient restart
 }
 
-# 停止节点
+# 8.停止节点
 function stop_service() {
 	service ceremonyclient stop
 }
 
-# 卸载节点
+# 9.卸载节点
 function clear_service() {
 	service ceremonyclient stop
 	rm -rf /root/ceremonyclient
 }
 
+
+
 # 主菜单
 function main_menu() {
 	echo "1. 安装服务节点"
-    echo "2. 查看节点日志"
-    echo "3. 重启节点"
-    echo "4. 停止节点"
-    echo "5. 卸载节点"
+    echo "2. 查看节点日志"  
+    echo "3. 配置监听端口8337和88338"
+    echo "4. 检查peerid"
+    echo "5. 获取PeerManifests信息"
+    echo "6. 检查节点是否可见"
+    echo "7. 重启节点"
+    echo "8. 停止节点"
+    echo "9. 卸载节点"
 
-    read -p "请输入选项（1-5）: " OPTION
+    read -p "请输入选项（1-9）: " OPTION
 
     case $OPTION in
     1) install_node ;;
     2) check_service_status ;;  
-    3) restart_service ;;
-	4) stop_service ;;
-	5) clear_service ;;
+	3) listen_addr ;;
+	4) check_peerid ;;
+	5) get_PeerManifests ;;
+	6) check_vaild ;;
+	7) restart_service ;;
+	8) stop_service ;;
+	9) clear_service ;;
     *) echo "无效选项。" ;;
     esac
 }
